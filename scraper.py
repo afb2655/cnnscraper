4836c1fb1936671d199a0b194c103a7402ffa4d9
theaiden28 <31458159+theaiden28@users.noreply.github.com>
2018-11-26 23:03:39 -0500

    Removed useless lines

--- scraper.py
+++ scraper.py
@@ -10,12 +10,7 @@ page = requests.get("http://rss.cnn.com/rss/cnn_topstories.rss")
 data = page.text
 
 soup = BeautifulSoup(data, 'lxml')
-print(soup.prettify())
-print(soup.find_all("a",class_="banner-text screaming-banner-text banner-text-size--char-44"))
-print(soup.find_all("a",href=True))
-print(soup.find_all("h4", class_="itemtitle"))
-for a in soup.find_all('a', href=True):
-    print ("Found the URL:", a['href'])
+
 
 Trumps = 0
 y = 0
@@ -30,20 +25,16 @@ for y in range(len(soup.find_all("description"))):
     teststring = sublist.text
     for i in range(len(teststring)):
         character = sublist.text[i]
-        debugarray.append(character)
-        ''.join(debugarray)
         if character == "<":
             break
         tempray.append(teststring[i])
         i += 1
     if len(tempray) >= 1:
         tempray.append("\n")
-    print(''.join(tempray))
-    print(c.findall(''.join(tempray)))
     Trumps += len(c.findall(''.join(tempray)))
     y += 1
 
-print(Trumps)
+print(Trumps)                                   #number of trumps!
 
 
 
